5d33d6c2a250d9bfb1d212af2a0a936c120ca8f3
aldrinn <48447138+aldringordon@users.noreply.github.com>
2023-03-31 20:39:11 +0800

    feat: call weather api and display in celsius

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+  
+  constructor(private weatherService: WeatherService) { }
+
+  convertFarToCelcius(far: number): number {
+    return (far - 32) * 5 / 9;
+  }
+
+  weatherData?: WeatherData;
+
+  temperature?: number;
+  maxTemperature?: number;
+  minTemperature?: number;
+  humidity?: number;
+  windSpeed?: number;
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Perth')
+    .subscribe({
+      next: (data) => {
+        this.weatherData = data;
+        this.temperature = this.convertFarToCelcius(data.main.temp);
+        this.maxTemperature = this.convertFarToCelcius(data.main.temp_max);
+        this.minTemperature = this.convertFarToCelcius(data.main.temp_min);
+        this.humidity = data.main.humidity;
+        this.windSpeed = data.wind.speed;
+        console.log(data);
+      }
+    });
+
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '',
+    XRapidAPIHostHeaderName: 'X-RapiAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+};
